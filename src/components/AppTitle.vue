b809cc707f46bd8577f460ecddc9da0f79957945
GiacomoMilitello <g.militello95@gmail.com>
2024-02-06 20:54:06 +0100

    title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+    name: "AppTitle"
+    }
+</script>
+
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+
+<style scoped>
+    h1{
+        width: 80%;
+        margin: 100px auto;
+        text-align: center;
+        color: rgb(255, 255, 255);
+        background-color: rgba(0, 0, 0, 0.7);
+        padding-block: 50px;
+        border-radius: 20px;
+    }
+</style>
